00c72d67a5192e55047d45b772d9c04520d4edc6
Robert Schaller <email@robertschaller.com>
2015-03-26 22:04:05 +0100

    new apk

---
+++ src/com/sinitphone/android/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.sinitphone.android;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+        
+    }
+    
+}
